4529633b64542015a53209bb704d254ff5b40ae1
marcourp <marcourp@morrisville.edu>
2019-09-03 16:44:04 -0400

    S2L13 finished

--- Marcoux-CITA312-F19/Assets/Scripts/Mover.cs
+++ Marcoux-CITA312-F19/Assets/Scripts/Mover.cs
@@ -7,17 +7,25 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    Ray lastRay;
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+    } // Update
 
-        GetComponent<NavMeshAgent>().destination = target.position;
-    }
-}
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    } // MoveToCursor
+
+} // class Mover
