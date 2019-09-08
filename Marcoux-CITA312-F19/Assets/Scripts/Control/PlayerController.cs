e4831d79b66679a2ee403cf5bc84b186ef65e4be
marcourp <marcourp@morrisville.edu>
2019-09-08 10:24:31 -0400

    S4L25 - Namespaces to see Dependencies

--- Marcoux-CITA312-F19/Assets/Scripts/Control/PlayerController.cs
+++ Marcoux-CITA312-F19/Assets/Scripts/Control/PlayerController.cs
@@ -1,26 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using RPG.Movement;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace RPG.Control
 {
-
-    private void Update()
+    public class PlayerController : MonoBehaviour
     {
-        if (Input.GetMouseButton(0))
-        {
-            MoveToCursor();
-        }
-    }
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
-        {
-            GetComponent<Mover>().MoveTo(hit.point);
-        }
-    } // MoveToCursor
 
-} // PlayerController
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        } // MoveToCursor
+
+    } // PlayerController
+
+}
--- Marcoux-CITA312-F19/Assets/Scripts/Movement/Mover.cs
+++ Marcoux-CITA312-F19/Assets/Scripts/Movement/Mover.cs
@@ -1,34 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    [SerializeField] Transform target;
-
-
-    
-    void Update()
+    public class Mover : MonoBehaviour
     {
-       UpdateAnimator();
-    } // Update
+        [SerializeField] Transform target;
 
-    
+        void Update()
+        {
+            UpdateAnimator();
+        } // Update
 
-    public void MoveTo(Vector3 destination)
-    {
-        GetComponent<NavMeshAgent>().destination = destination;
-    }
 
-    private void UpdateAnimator()
-    {
-        // get velocity from NavMeshAgent
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        // transform the direction to be a local velocity
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
-    }
 
-} // class Mover
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        private void UpdateAnimator()
+        {
+            // get velocity from NavMeshAgent
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            // transform the direction to be a local velocity
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+    } // class Mover
+}
