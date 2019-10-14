70bd254f2e6eaca88b96e97fdba578ae073a222b
marcourp <marcourp@morrisville.edu>
2019-10-14 15:47:34 -0400

    S7L73 - Cross Scene References

--- Marcoux-CITA-312-F19/Assets/Scripts/SceneManagement/Portal.cs
+++ Marcoux-CITA-312-F19/Assets/Scripts/SceneManagement/Portal.cs
@@ -9,8 +9,14 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -22,6 +28,12 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set.");
+                yield break;
+            }
+
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
@@ -42,7 +54,12 @@ namespace RPG.SceneManagement
         {
            foreach (Portal portal in FindObjectsOfType<Portal>())
             {
+                // only return the portal if it is not this portal and is the correct destination
                 if (portal == this)
+                {
+                    continue;
+                }
+                if (portal.destination != destination)
                 {
                     continue;
                 }
