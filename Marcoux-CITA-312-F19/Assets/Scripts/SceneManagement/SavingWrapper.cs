f89fa1fd9069e6583d2ef5e4cd65ea1280c09e55
marcourp <marcourp@morrisville.edu>
2019-10-14 19:43:31 -0400

    S8L79 - Triggering Saving and Loading

---
+++ Marcoux-CITA-312-F19/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using RPG.Saving;
+using System;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+        
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            // call to the Saving System and tell it to Save
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+
+        private void Load()
+        {
+            // call to the Saving System and tell it to Load
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+    }
+}
