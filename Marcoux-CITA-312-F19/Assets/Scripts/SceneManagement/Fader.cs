f6bc232157614c81f4bd724262f3efccaf919e3b
marcourp <marcourp@morrisville.edu>
2019-10-14 17:12:38 -0400

    S7L75 - Nested Co-Routines for Fading

---
+++ Marcoux-CITA-312-F19/Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            // for testing purposes
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            yield return FadeIn(1f);
+        }
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0) 
+            {
+                // moving alpha towards 0
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        } // FadeIn
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1) 
+            {
+                // moving alpha towards 1
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        } // FadeOut
+    } // class Fader
+}
