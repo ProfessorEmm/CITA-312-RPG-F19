34aa798d72e8e5e87c43c770d9d942689aa52d21
marcourp <marcourp@morrisville.edu>
2019-10-14 21:36:37 -0400

    S8L81 - Challenge: Saveable Health

--- Marcoux-CITA-312-F19/Assets/Scripts/Core/Health.cs
+++ Marcoux-CITA-312-F19/Assets/Scripts/Core/Health.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Core  
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
        [SerializeField] float healthPoints = 100f;
 
         bool isDead = false;
 
+        
         public bool IsDead()
         {
             return isDead;
@@ -33,6 +33,21 @@ namespace RPG.Core
             GetComponent<Animator>().SetTrigger("die");
             GetComponent<ActionScheduler>().CancelCurrentAction();
         }
+        public object CaptureState()
+        {
+            return healthPoints;
+        }
+        public void RestoreState(object state)
+        {
+            healthPoints = (float)state;
+
+            // ensure that the dead stay dead when restoring a state
+            if (healthPoints == 0)
+            {
+                Die();
+            }
+        }
+
     } // class Health
 
 } // namespace
